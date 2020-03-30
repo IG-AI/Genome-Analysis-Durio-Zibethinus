7d390f7878fedd9243467f72e0858f268021475a
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-03-30 23:28:10 +0200

    Added all scripts to one file

--- scripts/PacBio-Assemble/PacBio-DNA-Assemble.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-canu -assemble \
-  -p durio_zibethinus -d PacBio \
-  genomeSize=226.1g \
-  correctedErrorRate=0.039 \
-  -pacbio-corrected PacBio/durio_zibethinus.trimmedReads.fasta.gz
