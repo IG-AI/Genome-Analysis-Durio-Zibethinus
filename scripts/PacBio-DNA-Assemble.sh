a90556d6fc4e6bfaf86af60266d3ab0f12fc7b0d
Daniel Ågstrand <daniagst@rackham2.uppmax.uu.se>
2020-03-30 23:47:11 +0200

    Updated PacBio_Assemble script

--- scripts/PacBio-DNA-Assemble.sh
+++ scripts/PacBio-DNA-Assemble.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/bin/bash -l
+
+SBATCH -A g2020008
+SBATCH -p core
+SBATCH -n 2
+SBATCH -t 12:00:00
+SBATCH -J PacBio_Assembly
+SBATCH --mail-type=ALL
+SBATCH --mail-user daniel.agstrand.5971@student.uu.se
+
+module load bioinfo-tools
+module load canu
 
 canu -correct \
   -p durio_zibethinus -d PacBio \
-  genomeSize=226.1g \
+  genomeSize=715.23m \
   -pacbio-raw  /proj/g2020008/5_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz
 
 canu -trim \
   -p durio_zibethinus -d PacBio \
-  genomeSize=226.1g \
+  genomeSize=715.23m \
   -pacbio-corrected PacBio/durio_zibethinus.correctedReads.fasta.gz
 
 canu -assemble \
   -p durio_zibethinus -d PacBio \
-  genomeSize=226.1g \
+  genomeSize=715.23m \
   correctedErrorRate=0.039 \
   -pacbio-corrected PacBio/durio_zibethinus.trimmedReads.fasta.gz
