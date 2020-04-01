111466edca6afdb114456473a451dc3e3736c67c
Daniel Ågstrand <daniel.agstrand.5971@student.uu.se>
2020-04-01 15:27:17 +0200

    Updated script

--- scripts/PacBio-DNA-Assemble.sh
+++ scripts/PacBio-DNA-Assemble.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A g2020008
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 24:00:00
 #SBATCH -J PacBio_Assembly
 #SBATCH --mail-type=ALL
@@ -13,19 +13,24 @@ module load canu
 
 canu -correct \
   -p durio_zibethinus -d PacBio \
-  genomeSize=715.23m \
-  executiveThreads=2 \
+  genomeSize=26551601 \
+  executiveThreads=8 \
+  executiveMemory=25.6 \
+  stopOnLowCoverage=10 \
   -pacbio-raw  /proj/g2020008/5_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz
 
 canu -trim \
   -p durio_zibethinus -d PacBio \
-  genomeSize=715.23m \
-  executiveThreads=2 \
+  genomeSize=26551601 \
+  executiveThreads=8 \
+  executiveMemory=25.6 \
+  stopOnLowCoverage=10 \
   -pacbio-corrected PacBio/durio_zibethinus.correctedReads.fasta.gz
 
 canu -assemble \
   -p durio_zibethinus -d PacBio \
-  genomeSize=715.23m \
-  correctedErrorRate=0.039 \
-  executiveThreads=2 \
+  genomeSize=26551601m \
+  executiveThreads=8 \
+  executiveMemory=25.6 \
+  stopOnLowCoverage=10 \
   -pacbio-corrected PacBio/durio_zibethinus.trimmedReads.fasta.gz
