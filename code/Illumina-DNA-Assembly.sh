96e2e8a79f07d2f0f617c11fa8d973511fe24a6f
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-04-15 23:50:00 +0200

    Updated Illumina assembly

--- code/Illumina-DNA-Assembly.sh
+++ code/Illumina-DNA-Assembly.sh
@@ -4,39 +4,47 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 12:00:00
-#SBATCH -J Generete_BAM_file
+#SBATCH -J Illumina_Assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 
 module load bioinfo-tools
 module load bwa/0.7.17
 module load samtools/1.10
+module load Pilon/1.22
 
-# Generate BAM-file
+# Generate SAM-file
 bwa index \
  -p data/references_data/PacBio_ref \
  -a bwtsw  \
  data/assemble_data/PacBio/durio_zibethinus.contigs.fasta
 
-bwa aln \
+bwa mem \
  -t 4 \
  data/references_data/PacBio_ref \
  data/raw_data/illumina_data/SRR6058604_scaffold_06.1P.fastq.gz > data/align_data/SRR6058604_scaffold_06.1P.sai
 
-bwa aln \
+bwa mem \
  -t 4 \
  data/references_data/PacBio_ref \
- data/raw_data/illumina_data/SRR6058604_scaffold_06.2P.fastq.gz > data/align_data/SRR6058604_scaffold_06.2P.sai
+ data/raw_data/illumina_data/SRR6058604_scaffold_06.2P.fastq.gz \
+ data/raw_data/illumina_data/SRR6058604_scaffold_06.1P.fastq.gz > data/align_data/SRR6058604_scaffold_06.sam
 
-bwa sampe \
- data/references_data/PacBio_ref \
- data/align_data/SRR6058604_scaffold_06.1P.sai \
- data/align_data/SRR6058604_scaffold_06.2P.sai \
- data/raw_data/illumina_data/SRR6058604_scaffold_06.1P.fastq.gz \
- data/raw_data/illumina_data/SRR6058604_scaffold_06.2P.fastq.gz > data/align_data/SRR6058604_scaffold_06.sam
-
+# Generate BAM-file
 samtools view \
  -bT \
  data/assemble_data/PacBio/durio_zibethinus.contigs.fasta \
  data/align_data/SRR6058604_scaffold_06.sam > data/align_data/SRR6058604_scaffold_06.bam 
+
+samtools sort \
+ -O bam \
+ -o data/align_data/SRR6058604_scaffold_06.sorted.bam \
+ -T temp data/align_data/SRR6058604_scaffold_06.bam 
+
+# Assemble genome
+pilon \
+ --bam data/align_data/SRR6058604_scaffold_06.bam \
+ --outdir data/assemble_data/Illumina \
+ --threads 4 \
+ --diploid
 
