41344630e3ff969c06ee1d2bbdcd3952a54cc519
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-04-16 16:36:44 +0200

    Added script for transcriptome assembly

---
+++ code/Transcriptome_Assembly.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 12:00:00
+#SBATCH -J Transcriptome_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user daniel.agstrand.5971@student.uu.se
+
+module load bioinfo-tools
+module load trinity/2.9.1
+
+Trinity \
+ --seqType fa \
+ --max_memory 25.6G \
+ --left data/raw_data/transcriptome/trimmed/*06.1* \
+ --right data/raw_data/transcriptome/trimmed/*06.2* \
+ --CPU 8 \
+ --output data/assmemble_data/Transcriptome
+
+
+
+
+
