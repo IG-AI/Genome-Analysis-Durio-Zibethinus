b09f6193dbec66144dd576217d534cb89b3d14a2
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-04-21 22:50:56 +0200

    Transcriptome assambly script updated

--- code/Transcriptome_Assembly.sh
+++ code/Transcriptome_Assembly.sh
@@ -3,14 +3,46 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 12:00:00
-#SBATCH -J Transcriptome_assembly
+#SBATCH -t 24:00:00
+#SBATCH -J Transcriptome_BAM-file
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 
 module load bioinfo-tools
-module load trinity/2.9.1
+module load star
+module load trinity
 
+dir=data/raw_data/transcriptome/trimmed
+
+# Generate index
+STAR \
+ --runMode genomeGenerate \
+ --runThreadN 8 \
+ --genomeDir data/align_data/Transcriptome/star \
+ --genomeFastaFiles data/assemble_data/Illumina/durio_zibethinus.fasta
+
+# Mapping reads
+STAR \
+ --runThreadN 8 \
+ --readFilesIn $dir/SRR6040092_scaffold_06.1.fastq.gz,$dir/SRR6040092_scaffold_06.2.fastq.gz,$dir/SRR6040093_scaffold_06.1.fastq.gz,$dir/SRR6040093_scaffold_06.2.fastq.gz,$dir/SRR6040094_scaffold_06.1.fastq.gz,$dir/SRR6040094_scaffold_06.2.fastq.gz,$dir/SRR6040096_scaffold_06.1.fastq.gz,$dir/SRR6040096_scaffold_06.2.fastq.gz,$dir/SRR6040097_scaffold_06.1.fastq.gz,$dir/SRR6040097_scaffold_06.2.fastq.gz,$dir/SRR6156066_scaffold_06.1.fastq.gz,$dir/SRR6156066_scaffold_06.2.fastq.gz,$dir/SRR6156067_scaffold_06.1.fastq.gz
+,$dir/SRR6156067_scaffold_06.2.fastq.gz
+,$dir/SRR6156069_scaffold_06.1.fastq.gz
+,$dir/SRR6156069_scaffold_06.2.fastq.gz \
+ --outFileNamePrefix data/align_data/Transcriptome/star/durio_zibethinus \
+ --outSAMtype BAM Unsorted SortedByCoordinate \
+ --readFilesCommand gunzip -c \
+ --outSAMunmapped None
+
+# Assamble reads
+Trinity \
+ --genome_guided_bam data/align_data/Transcriptome/star/*.bam \
+ --max_memory 25G \
+ --CPU 8 \
+ --genome_guided_max_intron 10000 \
+ --output data/assemble_data/Transcriptome/trinity
+
+# De novo assembly
+'
 Trinity \
  --seqType fa \
  --max_memory 25G \
@@ -18,3 +50,4 @@ Trinity \
  --right data/raw_data/transcriptome/trimmed/*06.2* \
  --CPU 8 \
  --output data/assemble_data/Transcriptome/trinity
+'
