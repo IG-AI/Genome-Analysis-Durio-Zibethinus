b872a76e7441060088be535be992dac2b0b486ef
Daniel Ågstrand <daniel.agstrand.5971@student.uu.se>
2020-05-08 14:49:33 +0200

    Added annotation script

---
+++ code/Annotation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 24:00:00
+#SBATCH -J Annotation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user daniel.agstrand.5971@student.uu.se
+
+module load bioinfo-tools
+module load maker 
+
+maker -c 8 maker_opts.ctl maker_bopts.ctl maker_exe.ctl
